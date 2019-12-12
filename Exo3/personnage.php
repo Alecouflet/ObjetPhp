a861f08ad0853cabbbfed56fd0c4160b2bb7e88e
Alex80260 <alecouflet@la-providence.net>
2019-12-12 11:37:19 +0100

    exo 3

---
+++ Exo3/personnage.php
@@ -0,0 +1,18 @@
+<?php 
+    class Personnage
+    {
+        // propriété :
+        private $_Pseudo;
+        private $_Vie;
+
+        // méthode :
+        public function __construct($SonPseudo,$SaVie){
+            $this->_Vie = $SaVie;
+            $this->_Pseudo = $SonPseudo;
+        }
+        public function Afficher(){
+            echo "<p>La vie du perso est ".$this->_Vie."<p>";
+            echo "<p>Son pseudo est ".$this->_Pseudo."<p>";
+        }
+    }
+?>
