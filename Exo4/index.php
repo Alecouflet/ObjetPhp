6cf3657651e138560651a19b29b4709c2b57f3ac
Alex80260 <alecouflet@la-providence.net>
2019-12-20 10:10:46 +0100

    exo4

--- Exo4/index.php
+++ Exo4/index.php
@@ -1,13 +1,16 @@
-<?php require ("personnage.php"); ?>
+<?php require ("joueur.php"); ?>
 <link rel="stylesheet" href="user.css">
 <html>
  <head>
 </head>
 <body>
-    <h3>Exercice 3<h3>
+    <h3>Exercice 4<h3>
 
-<?php $Perso1 = new Personnage("Julien",100);
-    $Perso1->Afficher();
+<?php 
+$Perso1 = new joueur("Julien",100);
+$Perso2 = new joueur("Alexis",100);
+$Perso1->Attaquer($Perso2);
+$Perso2->Attaquer($Perso1);
 ?>
 <?php echo  highlight_file('./'.basename(__FILE__),true);?>
 </body>
--- Exo4/joueur.php
+++ Exo4/joueur.php
@@ -6,13 +6,20 @@
         private $_Vie;
 
         // méthode :
-        public function __construct($SonPseudo,$SaVie){
+        public function __construct($SonPseudo,$SaVie)
+        {
             $this->_Vie = $SaVie;
             $this->_Pseudo = $SonPseudo;
         }
-        public function Afficher(){
-            echo "<p>La vie du perso est ".$this->_Vie."<p>";
-            echo "<p>Son pseudo est ".$this->_Pseudo."<p>";
+        public function Attaquer($Attaque1)
+        {
+            echo '<p>'.$this->_Pseudo.' attaque<p>';
+            $Attaque1->Defendre(50);
         }
+        public function Defendre($Def){
+            $this->_vie = $Def;
+            echo '<p>'.$this->_Pseudo.' a plus que '.$this->_vie.' hp.<p>';
+        }
+    
     }
 ?>
