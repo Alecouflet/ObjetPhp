8d2480c0ebd8c1f8cb9efea0944f7159eed7b1af
Alex80260 <alecouflet@la-providence.net>
2019-12-20 13:45:02 +0100

    index

---
+++ index.php
@@ -0,0 +1,30 @@
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <title>Acceuil</title>
+</head>
+
+<body>
+<h1 class=titre>
+    <?php echo "Alexis Lecouflet SN1 : Index des exercices Objet PHP"; ?>
+</h1>
+
+<nav>
+    <ul>
+        <li class="deroulant"><a href="#">Exercices PHP Objet &ensp;</a>
+            <ul class="sous">
+                <li><a href="/Lecouflet/ObjetPHP/Exo1">Exercice Objet PHP 1</a></li>
+                <li><a href="/Lecouflet/ObjetPHP/Exo2">Exercice Objet PHP 2</a></li>
+                <li><a href="/Lecouflet/ObjetPHP/Exo3">Exercice Objet PHP 3</a></li>
+                <li><a href="/Lecouflet/ObjetPHP/Exo4">Exercice Objet PHP 4</a></li>
+            </ul>
+        </li>
+        </ul>
+    </nav>
+</body>
+
+</html>
